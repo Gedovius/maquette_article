89c6708bca292d09b81cc0178f8f9dea10ecf0e9
gedovius <annegedovius@hotmail.fr>
2020-04-30 19:20:16 +0200

    mise à jour

---
+++ view.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Maquette Abonnement</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+<nav class="navbar navbar-expand-lg">
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">International</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">France</a>
+            </li>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Grand-Est</a>
+            </li>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Economie</a>
+            </li>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Culture</a>
+            </li>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Le dossier de l'Edito</a>
+            </li>
+        </ul>
+    </nav>
+<br>
+
+<form>
+    <div class="form-row">
+        <div class="form-group col-md-6">
+        <label for="inputEmail4"></label>
+        <input type="text" class="form-control" id="user_name">
+        </div>
+        <br><div class="form-group col-md-6">
+        <label for="inputPassword4"></label>
+        <input type="password" class="form-control" id="user_password">
+        </div>
+        <div class="form-group col-md-6">
+        <label for="inputPassword4"></label>
+        <input type="password" class="form-control" id="user_password">
+        </div>
+    </div>
+
+    <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
+  <div class="input-group mb-2 mr-sm-2">
+    <div class="input-group-prepend">
+      <div class="input-group-text">@</div>
+    </div>
+    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
+  </div>
+
+
+    <br>
+<div class="form-check">
+  <label class="form-check-label">
+    <input type="checkbox" class="form-check-input" name="" id="" value="checkedValue" checked>PAYPAL</input><br>
+    <input type="checkbox" class="form-check-input" name="" id="" value="checkedValue" checked>CB VISA</input><br>
+    <input type="checkbox" class="form-check-input" name="" id="" value="checkedValue" checked>MASTERCARD</input><br>
+
+  </label>
+</div>
+<br>
+    <div class="form-group">
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck">
+        <label class="form-check-label" for="gridCheck">Remember me</label>
+        </div>
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Soumettre</button>
+
+
+<!-- Optional JS - Jquery - Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
